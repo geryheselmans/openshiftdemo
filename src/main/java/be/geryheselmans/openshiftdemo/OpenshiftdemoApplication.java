34c40b316f7838a36113b6f9bc163150bf8f2245
geryheselmans <gery.heselmans@hotmail.com>
2018-02-02 21:46:07 +0100

    Got spring working

--- src/main/java/be/geryheselmans/openshiftdemo/OpenshiftdemoApplication.java
+++ src/main/java/be/geryheselmans/openshiftdemo/OpenshiftdemoApplication.java
@@ -1,10 +1,20 @@
 package be.geryheselmans.openshiftdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class OpenshiftdemoApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftdemoApplication.class, args);
